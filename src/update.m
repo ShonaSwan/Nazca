3eed06ed7bdf5a370f01fc3dddbf4d9283f2c8fe
Tobias Keller <kellertobs@gmail.com>
2025-07-09 08:24:19 +0100

    minor updates to coefficient model for internal consistency

--- src/update.m
+++ src/update.m
@@ -68,10 +68,6 @@ if Nz==1; Pt    = max(1e7,(1-alpha).*Pt + alpha.*(Ptop.*ones(size(Tp)) + Pcouple
 end
 upd_Pt = Pt-Pti;
 
-% update effective constituent sizes
-dm = dm0.*(1-mu ).^0.5;
-dx = dx0.*(1-chi).^0.5;
-
 % update pure phase viscosities
 etam   = reshape(Giordano08(reshape(cm_oxd_all,Nz*Nx,9),T(:)-273.15),Nz,Nx);
 etax0  = reshape(prod(cal.etax0(1:end-1).^reshape(chi_mem(:,:,1:end-1)+eps,Nz*Nx,cal.nmem-1),2),Nz,Nx);
@@ -79,12 +75,11 @@ etax   = etax0 .* exp(cal.Eax./(8.3145.*T)-cal.Eax./(8.3145.*(Tref+273.15)));
 
 % get coefficient contrasts
 kv = permute(cat(3,etax,etam),[3,1,2]);
-kf = permute(cat(3,dx.^2./etax,dm.^2./etam),[3,1,2]);
+kf = permute(cat(3,dx0.^2./etax,dm0.^2./etam),[3,1,2]);
 Mv = permute(repmat(kv,1,1,1,2),[4,1,2,3])./permute(repmat(kv,1,1,1,2),[1,4,2,3]);
 Mf = permute(repmat(kf,1,1,1,2),[4,1,2,3])./permute(repmat(kf,1,1,1,2),[1,4,2,3]);
 
 % get permission weights
-dd = max(eps^0.5,min(1-eps^0.5,permute(cat(3,dx ,dm ),[3,1,2])));
 ff = max(eps^0.5,min(1-eps^0.5,permute(cat(3,chi,mu ),[3,1,2])));
 FF = permute(repmat(ff,1,1,1,2),[4,1,2,3]);
 Sf = (FF./cal.BB).^(1./cal.CC);  Sf = Sf./sum(Sf,2);
@@ -93,12 +88,12 @@ Xf = sum(cal.AA.*Sf,2).*FF + (1-sum(cal.AA.*Sf,2)).*Sf;
 % get momentum flux and transfer coefficients
 thtv = squeeze(prod(Mv.^Xf,2));
 Kv   = ff.*kv.*thtv;
-Cv   = Kv.*(1-ff)./dd.^2;
+Cv   = Kv./[dx0; dm0].^2;
 
 % get volume flux and transfer coefficients
 thtf = squeeze(prod(Mf.^Xf,2));
 Kf   = ff.*kf.*thtf;
-Cf   = Kf.*(1-ff)./dd.^2;
+Cf   = Kf./[dx0; dm0].^2;
 
 % get effective viscosity
 eta = squeeze(sum(Kv,1)); if Nx==1; eta0 = eta0.'; end
@@ -106,12 +101,9 @@ eta = squeeze(sum(Kv,1)); if Nx==1; eta0 = eta0.'; end
 % get segregation cofficients
 Ksgr   = ff./Cv;
 
-Ksgr_x = squeeze(Ksgr(1,:,:)) + eps^2; if Nx==1; Ksgr_x = Ksgr_x.'; end
-Ksgr_m = squeeze(Ksgr(2,:,:)) + eps^2; if Nx==1; Ksgr_m = Ksgr_m.'; end
-
 % traditional two-phase coefficients
-KD     = max(eps^0.5,mu ).^2./squeeze(Cv(2,:,:));  % melt segregation coeff
-zeta   = max(eps^0.5,chi).^2./squeeze(Cf(1,:,:));  % solid compaction coeff
+KD     = max(mulim,mu ).^2./squeeze(Cv(2,:,:));  % melt segregation coeff
+zeta   = max(mulim,chi).^2./squeeze(Cf(1,:,:));  % solid compaction coeff
 
 %Extracted bounday conditions
 twophs = double(mu(icz,icx)>=mulim);
@@ -159,7 +151,6 @@ Re     = Vel.*D/10./( eta       ./rho    );
 Rum    = abs(wm(1:end-1,2:end-1)+wm(2:end,2:end-1))/2./Vel;
 Pr     = (eta./rho)./((kT+ks.*T)./rho./cP);
 Sc     = (eta./rho)./( kc                 );
-deltam = sqrt(mu .*Ksgr_m.*eta./(1-chi));
 
 % update stresses
 txx = eta   .* exx;                                                        % x-normal stress
@@ -179,8 +170,7 @@ else
          + exx.*txx + ezz.*tzz ...
          + 2.*(exz(1:end-1,1:end-1)+exz(2:end,1:end-1)+exz(1:end-1,2:end)+exz(2:end,2:end))./4 ...
             .*(txz(1:end-1,1:end-1)+txz(2:end,1:end-1)+txz(1:end-1,2:end)+txz(2:end,2:end))./4 ...
-         +  mu./Ksgr_m .* ((wm(1:end-1,2:end-1)+wm(2:end,2:end-1))./2).^2 ...
-         + chi./Ksgr_x .* ((wx(1:end-1,2:end-1)+wx(2:end,2:end-1))./2).^2;
+         +  KD .* ((wm(1:end-1,2:end-1)+wm(2:end,2:end-1))./2).^2;;
 end
 
 UDtime = UDtime + toc;
